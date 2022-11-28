6c9519eee8d39c40c5090ef42bc5d23652d64e0a
Warizz Yutanan <warizz.yutanan@gmail.com>
2022-11-28 15:38:23 +0700

    ...

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -2,7 +2,6 @@ import "../public/global.css";
 
 import type { AppProps } from "next/app";
 
-// This default export is required in a new `pages/_app.js` file.
 export default function Web({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
