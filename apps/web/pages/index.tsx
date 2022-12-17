8c9beda252558283727c8c3d26114d2730d95ae9
Warizz Yutanan <warizz.yutanan@gmail.com>
2022-12-17 21:51:35 +0700

    feat(homepage): title & description

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,15 +1,25 @@
+import Head from "next/head";
 import Link from "next/link";
 
 import BlogLayout from "../components/BlogLayout";
 
 export default function Web() {
   return (
-    <BlogLayout breadcrumbs={[]} h1={<h1>Warizz</h1>}>
-      <ul>
-        <li>
-          <Link href="/posts">blogs</Link>
-        </li>
-      </ul>
-    </BlogLayout>
+    <>
+      <Head>
+        <title>Warizz Yutanan</title>
+        <meta
+          name="description"
+          content="The homepage of Warizz Yutanan personal website."
+        />
+      </Head>
+      <BlogLayout breadcrumbs={[]} h1={<h1>Warizz</h1>}>
+        <ul>
+          <li>
+            <Link href="/posts">blogs</Link>
+          </li>
+        </ul>
+      </BlogLayout>
+    </>
   );
 }
