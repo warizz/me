1a36371c10928eb62a971ba47a15d50d4f238339
Warizz Yutanan <warizz.yutanan@gmail.com>
2022-12-18 19:00:36 +0700

    chore: add GA pt.2

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -1,18 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
+const ID = "G-F7QMBWEZW5";
+
 export default function Document() {
   return (
     <Html>
       <Head>
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-F7QMBWEZW5"
+          src={"https://www.googletagmanager.com/gtag/js?id=" + ID}
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
-          {
-            'function gtag(){window.dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","GA_MEASUREMENT_ID");'
-          }
+          {`function gtag(){window.dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","${ID}");`}
         </Script>
       </Head>
       <body>
