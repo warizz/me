629cbb77eeb35181f2d9a75dab93cfc543ee9126
Warizz Yutanan <warizz.yutanan@gmail.com>
2023-01-02 16:38:08 +0700

    chore: centralized configs (#75)

--- apps/naminana/pages/_document.tsx
+++ apps/naminana/pages/_document.tsx
@@ -1,21 +1,21 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
-const ID = "G-F7QMBWEZW5";
+import { GA_ID } from "../app.config";
 
-export default function Document() {
+export default function _Document() {
   return (
     <Html>
       <Head>
         {!process.env.NEXT_PUBLIC_IS_DISABLED_GA ? (
           <>
             <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${ID}`}
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
               strategy="afterInteractive"
               data-testid="ga_lib"
             />
             <Script id="ga_datalayer" strategy="afterInteractive">
-              {`function gtag(){window.dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","${ID}");`}
+              {`function gtag(){window.dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","${GA_ID}");`}
             </Script>
           </>
         ) : null}
--- apps/warizzyutanan/pages/index.tsx
+++ apps/warizzyutanan/pages/index.tsx
@@ -1,19 +1,21 @@
 import Link from "next/link";
 import Page from "shared/Page";
 
+import { homeConfig } from "../app.config";
+
 export default function Web() {
   return (
     <Page
       meta={{
-        title: "Warizz Yutanan",
-        description: "Warizz's whatever",
+        title: homeConfig.title,
+        description: homeConfig.description,
         robots: "index, follow",
       }}
       layout={{
         breadcrumbs: [],
         h1: (
-          <h1 className="text-red-800 dark:text-amber-500">
-            {"Warizz's whatever"}
+          <h1 className="text-primary dark:text-primary-invert">
+            {homeConfig.h1}
           </h1>
         ),
       }}
