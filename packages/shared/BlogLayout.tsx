e308ebc79a95af394fd235b47f0e326d7f944e8b
Warizz Yutanan <warizz.yutanan@gmail.com>
2023-01-04 11:19:12 +0700

    feat(naminana): "laparoscopic-surgery" (#82)

--- packages/shared/BlogLayout.tsx
+++ packages/shared/BlogLayout.tsx
@@ -16,7 +16,7 @@ export default function BlogLayout({ children, breadcrumbs, h1, date }: Props) {
     <div className="bg-white lg:pt-20 dark:bg-black min-h-screen ease-in duration-100">
       <article className="prose lg:prose-xl mx-auto p-4 font-serif dark:prose-invert">
         <ToolsBar
-          className="mb-3 lg:mb-0"
+          className="mb-3 lg:mb-2"
           breadcrumbs={[{ text: "home", href: "/" }].concat(breadcrumbs)}
         />
         {h1}
