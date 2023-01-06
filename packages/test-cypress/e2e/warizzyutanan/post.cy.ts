944a6fde8b4aff0bd5c03b531fb47c281d961ea3
Warizz Yutanan <warizz.yutanan@gmail.com>
2023-01-06 11:10:01 +0700

    ...

--- packages/test-cypress/e2e/warizzyutanan/post.cy.ts
+++ packages/test-cypress/e2e/warizzyutanan/post.cy.ts
@@ -3,4 +3,9 @@ describe("post", () => {
     cy.visit("http://localhost:3000/posts/resume");
     cy.get("h1").should("have.text", "Warizz Yutanan");
   });
+
+  it("should render code blocks with syntax-highlighting", () => {
+    cy.visit("http://localhost:3000/posts/dependency-injection-in-react");
+    cy.get("h1").should("have.text", "Dependency Injection in React");
+  });
 });
