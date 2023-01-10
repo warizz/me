5eabd188a97ee589a26475067313ebd7fde916f5
Warizz Yutanan <warizz.yutanan@gmail.com>
2023-01-10 13:22:31 +0700

    chore: gen-sitemap script (#100)

--- packages/shared/lib/parseToSitemap.ts
+++ packages/shared/lib/parseToSitemap.ts
@@ -1,6 +1,6 @@
 import type { IPost } from "shared/lib/getPostData";
 
-export default function generateSiteMap(DOMAIN: string, posts: IPost[]) {
+export default function parseToSitemap(DOMAIN: string, posts: IPost[]) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <url>
---
+++ packages/shared/lib/updateSitemap.ts
@@ -0,0 +1,28 @@
+import { readdirSync, writeFileSync } from "fs";
+import path from "path";
+
+import getPostData, { postsDirectory } from "shared/lib/getPostData";
+import parseToSitemap from "shared/lib/parseToSitemap";
+
+function _info(...args: unknown[]) {
+  console.info("[gen-sitemap]", ...args);
+}
+
+export default function updateSitemap(domain: string) {
+  const publicDir = path.join(process.cwd(), "public");
+  const sitemapFilePath = path.join(publicDir, "sitemap.xml");
+
+  _info("started ...");
+  _info(`publicDir "${publicDir}"`);
+
+  const posts = readdirSync(postsDirectory)
+    .map(getPostData)
+    .filter((post) => post.isPublished);
+
+  const sitemap = parseToSitemap(domain, posts);
+
+  _info(`write file "${sitemapFilePath}"`);
+  writeFileSync(path.join(publicDir, "sitemap.xml"), sitemap);
+
+  _info("finished ...");
+}
