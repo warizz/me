d4d3b7b3c4ae3b8926a5e20e9d026558d1b7d2d5
Warizz Yutanan <warizz.yutanan@gmail.com>
2023-01-10 09:13:41 +0700

    fix: sitemap (#95)

--- packages/shared/lib/generateSiteMap.ts
+++ packages/shared/lib/generateSiteMap.ts
@@ -3,7 +3,6 @@ import type { IPost } from "shared/lib/getPostData";
 export default function generateSiteMap(DOMAIN: string, posts: IPost[]) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
      <url>
        <loc>https://www.${DOMAIN}/</loc>
      </url>
