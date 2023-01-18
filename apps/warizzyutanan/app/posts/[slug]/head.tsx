a554b98d7e87d756306081ef797035ab1fa5b8aa
Warizz Yutanan <warizz.yutanan@gmail.com>
2023-01-18 11:57:38 +0700

    ...

--- apps/warizzyutanan/app/posts/[slug]/head.tsx
+++ apps/warizzyutanan/app/posts/[slug]/head.tsx
@@ -6,12 +6,10 @@ import { OWNER_NAME } from "../../../app.config";
 export default async function Head({ params }: { params: { slug: string } }) {
   const { title, description } = await getPostData(`${params.slug}.md`);
   return (
-    <>
-      <DefaultHeadTag
-        title={`${title} - ${OWNER_NAME}`}
-        description={description}
-        robots="index, follow"
-      />
-    </>
+    <DefaultHeadTag
+      title={`${title} - ${OWNER_NAME}`}
+      description={description}
+      robots="index, follow"
+    />
   );
 }
