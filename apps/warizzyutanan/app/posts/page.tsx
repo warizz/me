a7e57df8c06a8014bdf7f715470967207574a812
Warizz Yutanan <warizz.yutanan@gmail.com>
2023-01-19 09:23:49 +0700

    ...

--- apps/warizzyutanan/app/posts/page.tsx
+++ apps/warizzyutanan/app/posts/page.tsx
@@ -18,5 +18,7 @@ export default async function Page({ searchParams }: Props) {
   const tag =
     typeof searchParams?.tag === "string" ? searchParams?.tag : undefined;
 
+  console.log({ tag, posts });
+
   return <Posts posts={posts} title={`${OWNER_NAME}'s blog`} tag={tag} />;
 }
