b3c88208196ed8ea6926259cdcf3cb59cf800fa9
warizz.yut <warizz.yut@ascendcorp.com>
2023-05-12 12:15:23 +0700

    code-cov-demo

--- apps/warizzyutanan/pages/posts/index.tsx
+++ apps/warizzyutanan/pages/posts/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Posts from "shared/Posts";
 import { getStaticProps as _getStaticProps } from "shared/Posts.server";
 import type { IPostsPage } from "shared/Posts.server";
@@ -5,6 +6,13 @@ import type { IPostsPage } from "shared/Posts.server";
 type Props = IPostsPage;
 
 const PostsPage = (props: Props) => {
+  useEffect(() => {
+    if (process.env.IS_CONSERVATIVE) {
+      console.log("พลังประชารัฐ เบอร์ 37");
+    } else {
+      console.log("ก้าวไกล เบอร์ 31");
+    }
+  }, []);
   return <Posts {...props} />;
 };
 
