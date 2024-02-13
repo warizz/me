383b00446d524c8aa835723a2fb98c2d1b5c9cbb
warizz.yut <warizz.yut@ascendcorp.com>
2024-02-13 08:29:03 +0700

    ui

--- apps/warizzyutanan/app/logs/page.tsx
+++ apps/warizzyutanan/app/logs/page.tsx
@@ -1,3 +1,7 @@
+import { intervalToDuration, formatDuration } from "date-fns";
+import capitalize from "lodash/capitalize";
+import groupBy from "lodash/groupBy";
+import map from "lodash/map";
 import { Metadata } from "next";
 import ToolsBar from "shared/ToolsBar";
 
@@ -7,13 +11,13 @@ const csvPath = `/app/logs/logs.csv`;
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: `My movies`,
+    title: `Logs of my whatever - Warizz`,
   };
 }
 
 export default async function Page() {
   const list = await parseLogsCsv(csvPath);
-  console.log({ list });
+  const groupes = groupBy(list, "category");
 
   return (
     <>
@@ -25,15 +29,35 @@ export default async function Page() {
         ]}
       />
       <div>
-        <div className="bg-white dark:bg-black h-screen overflow-auto">
-          <div className="container p-8 mx-auto prose dark:prose-invert font-serif">
-            <ul>
-              {list.map((item) => {
-                return <li key={item.id}>{item.title}</li>;
-              })}
-            </ul>
-          </div>
-        </div>
+        <h1>Logs of my whatever.</h1>
+        {map(groupes, (list, key) => {
+          return (
+            <div key={key}>
+              <h2>{capitalize(key)}</h2>
+              <ul>
+                {list
+                  .filter((item) => !item.ended_at)
+                  .map((item) => {
+                    const duration = intervalToDuration({
+                      start: new Date(item.started_at),
+                      end: item.ended_at ? new Date(item.ended_at) : new Date(),
+                    });
+                    const formattedDuration = formatDuration(duration, {
+                      format: ["years", "months", "days"],
+                    });
+                    return (
+                      <li key={item.id}>
+                        <div>
+                          <strong className="mr-2">{item.title}</strong>
+                          <span className="font-mono">{`(${formattedDuration})`}</span>
+                        </div>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+          );
+        })}
       </div>
     </>
   );
