d5cd0906ca3f08d496e91f5d4d105a67f160317b
warizz.yut <warizz.yut@ascendcorp.com>
2025-01-06 17:06:57 +0700

    ...

--- packages/test-cypress/e2e/naminana/sitemap.cy.ts
+++
@@ -1,7 +0,0 @@
-describe("sitemap.xml", () => {
-  it("passes", () => {
-    cy.request("http://localhost:3001/sitemap.xml").then((res) => {
-      expect(res.status).to.eq(200);
-    });
-  });
-});
