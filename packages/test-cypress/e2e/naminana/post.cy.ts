d428ee9b2f9dc6e97d0575d44156aff7413c7cd6
Warizz Yutanan <warizz.yutanan@gmail.com>
2025-01-06 17:09:50 +0700

    chore: clean up unused projects (#1554)

--- packages/test-cypress/e2e/naminana/post.cy.ts
+++
@@ -1,9 +0,0 @@
-describe("post", () => {
-  it("passes", () => {
-    cy.visit("http://localhost:3001/posts/laparoscopic-surgery");
-    cy.get("h1").should(
-      "have.text",
-      "บันทึก ตัดเนื้องอกและมดลูก ของนามิ (Laparoscopic Surgery: Myomectomy + Hysteroscopy)"
-    );
-  });
-});
--- packages/test-cypress/e2e/naminana/posts.cy.ts
+++
@@ -1,6 +0,0 @@
-describe("posts", () => {
-  it("passes", () => {
-    cy.visit("http://localhost:3001/posts");
-    cy.get("h1").should("have.text", "Blogs");
-  });
-});
