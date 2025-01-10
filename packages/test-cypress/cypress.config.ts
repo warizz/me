89b8f4b4a45d2d48d91f9cdff4c27c422f4c96af
warizz.yut <warizz.yut@ascendcorp.com>
2025-01-10 09:26:58 +0700

    ...

--- packages/test-cypress/cypress.config.ts
+++ packages/test-cypress/cypress.config.ts
@@ -10,10 +10,6 @@ export default defineConfig({
   e2e: {
     supportFile: "support/e2e.ts",
     specPattern: "e2e/**/*.cy.{ts,tsx}",
-    setupNodeEvents(on, config) {
-      require("@cypress/code-coverage/task")(on, config);
-      return config;
-    },
     video: false,
   },
 });
