1a8e9eef8e57212aa2aecc9d1d5b939f06a4f125
warizz.yut <warizz.yut@ascendcorp.com>
2025-02-28 15:55:59 +0700

    feat(life-in-weeks): init
    x

--- apps/warizzyutanan/app/life-in-weeks/page.tsx
+++ apps/warizzyutanan/app/life-in-weeks/page.tsx
@@ -4,14 +4,16 @@ import React from "react";
 import weeks from "../../weeks.json";
 
 const startDate = new Date("1984-08-08");
-const today = new Date();
+const endDate = new Date(startDate);
+endDate.setFullYear(startDate.getFullYear() + 100);
 
 const getWeeksBetween = (start: Date, end: Date) => {
   const diffInMs = end.getTime() - start.getTime();
   return Math.floor(diffInMs / (7 * 24 * 60 * 60 * 1000));
 };
 
-const weeksCount = getWeeksBetween(startDate, today);
+const weeksCount = getWeeksBetween(startDate, endDate);
+const currentWeek = getWeeksBetween(startDate, new Date());
 
 const WeeklyTimeline = () => {
   return (
@@ -23,17 +25,19 @@ const WeeklyTimeline = () => {
           https://weeks.ginatrapani.org/
         </a>
       </p>
-      <div className="flex flex-wrap justify-evenly gap-1 font-sans text-sm">
+      <div className="flex flex-wrap justify-between gap-1 font-sans text-sm">
         {[...Array(weeksCount)].map((_, i) => {
           const weekDate = new Date(startDate);
           weekDate.setDate(startDate.getDate() + i * 7);
 
-          const event = weeks.find((e) => {
-            const eventDate = new Date(e.date);
+          const event = weeks.find((w) => {
+            const eventDate = new Date(w.date);
             const eventWeek = getWeeksBetween(startDate, eventDate);
             return eventWeek === i;
           });
           const isHighlighted = event && !event.is_private;
+          const isCurrentWeek = i === currentWeek;
+          const isFutureWeek = i > currentWeek;
 
           return (
             <div
@@ -41,11 +45,16 @@ const WeeklyTimeline = () => {
               content={isHighlighted ? (event?.detail ?? "") : ""}
               data-testid="week"
               className={clsx(
-                "h-5 rounded border flex items-center justify-center px-2",
+                "h-5 rounded border flex items-center justify-center px-2 grow",
                 {
                   "bg-blue-500 border-blue-700 text-white min-w-auto":
                     isHighlighted,
-                  "bg-gray-200 min-w-[16px]": !isHighlighted,
+                  "bg-[#FFAB5B] border-[#A31D1D] text-white min-w-auto":
+                    isCurrentWeek,
+                  "border-[#2DAA9E] bg-[#B4EBE6]":
+                    !isHighlighted && !isCurrentWeek && !isFutureWeek,
+                  "bg-gray-200":
+                    isFutureWeek && !isHighlighted && !isCurrentWeek,
                 },
               )}
             >
